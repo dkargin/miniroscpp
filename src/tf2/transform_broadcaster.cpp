c4f694b363f2ada1a3fb3c1535273897a127c3b6
Dmitry Kargin <dmitry.n.kargin@gmail.com>
2024-10-29 11:57:38 +0300

    Wrapped TransformBroadcaster into tf2 namespace

--- src/tf2/transform_broadcaster.cpp
+++ src/tf2/transform_broadcaster.cpp
@@ -48,10 +48,36 @@
 
 namespace miniros {
 
+namespace tf2 {
+
+struct TransformBroadcaster::Internal {
+  /// Internal reference to ros::Node
+  miniros::NodeHandle node;
+  miniros::Publisher publisher;
+
+  Internal() {}
+  Internal(NodeHandle& nh) : node(nh) {}
+};
+
 TransformBroadcaster::TransformBroadcaster()
 {
-  publisher_ = node_.advertise<tf2_msgs::TFMessage>("/tf", 100);
-};
+  m_internal.reset(new Internal());
+  init();
+}
+
+TransformBroadcaster::TransformBroadcaster(miniros::NodeHandle& node) {
+  m_internal.reset(new Internal(node));
+  init();
+}
+
+TransformBroadcaster::~TransformBroadcaster() {
+}
+
+void TransformBroadcaster::init(int sendQueueLength) {
+  if (!m_internal)
+    return;
+  m_internal->publisher = m_internal->node.advertise<tf2_msgs::TFMessage>("/tf", sendQueueLength);
+}
 
 void TransformBroadcaster::sendTransform(const geometry_msgs::TransformStamped & msgtf)
 {
@@ -63,13 +89,16 @@ void TransformBroadcaster::sendTransform(const geometry_msgs::TransformStamped &
 
 void TransformBroadcaster::sendTransform(const std::vector<geometry_msgs::TransformStamped> & msgtf)
 {
+  if (!m_internal)
+    return;
   tf2_msgs::TFMessage message;
   for (std::vector<geometry_msgs::TransformStamped>::const_iterator it = msgtf.begin(); it != msgtf.end(); ++it)
   {
     message.transforms.push_back(*it);
   }
-  publisher_.publish(message);
+  m_internal->publisher.publish(message);
 }
 
+} // namespace tf2
 
 } // namespace miniros
